c250963ca99806ef56e2a1252dcbf928be900f99
YunusEmre577 <yunusemres0001@gmail.com>
2024-08-08 19:24:59 +0300

    Update hero.jsx
    editing blur

--- components/About/hero.jsx
+++ components/About/hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function hero() {
   return (
     <section className="-translate-y-20 flex flex-col items-center justify-center">
-        <div className="absolute h-lvh w-32 blur-[900px] bg-indigo-50 pointer-events-none"></div>
+        <div className="absolute h-svh w-32 blur-[900px] bg-indigo-50 pointer-events-none"></div>
         <div className="flex flex-col items-center text-center">
         <h1 className="text-[60px] text-transparent font-bold bg-clip-text bg-gradient-to-br to-[#8c8ccc] from-[#e2e2f1]">About me</h1>
         <p className="max-w-6xl">Hello! My name is Yunus Emre GÜN. I'm 13 and I was born in the Balıkesir. My goal is to become a computer engineer and I am working hard towards this. I go to high school and I'm in the 9th grade. You can see my information below.</p>
