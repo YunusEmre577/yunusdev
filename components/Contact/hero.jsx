f063c398ff7de9632538d4c84ee20c9d99e5e550
YunusEmre577 <yunusemres0001@gmail.com>
2024-08-08 19:22:55 +0300

    Update hero.jsx
    blur editing

--- components/Contact/hero.jsx
+++ components/Contact/hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function hero() {
   return (
     <section className="-translate-y-20 flex flex-col items-center justify-center">
-        <div className="absolute h-lvh w-32 blur-[600px] bg-indigo-50 pointer-events-none"></div>
+        <div className="absolute h-lvh w-32 blur-[900px] bg-indigo-50 pointer-events-none"></div>
         <div className="flex flex-col items-center text-center">
         <h1 className="text-[60px] text-transparent font-bold bg-clip-text bg-gradient-to-br to-[#8c8ccc] from-[#e2e2f1]">Contact</h1>
         <p className="max-w-6xl">You can contact me by filling out the form below or you can reach me using the information below.</p>
